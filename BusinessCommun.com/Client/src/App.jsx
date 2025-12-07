48e7f44a5df901c67c839293d45a121910e1ee8d
Amisha Kumari <amishak187@gmail.com>
2025-12-07 21:54:40 +0530

    Added About page

--- BusinessCommun.com/Client/src/App.jsx
+++ BusinessCommun.com/Client/src/App.jsx
@@ -5,6 +5,14 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Login from './Pages/Login/Login.jsx'
 import Home from './Pages/Home/Home.jsx'
+<<<<<<< Updated upstream
+=======
+import CompanyDetail from './Component/Forms/Partner_Investor_Forms/Company_Details_Form/Comp_Detail_Form.jsx'
+import PartnerConnect from './Component/Forms/Partner_Investor_Forms/Partner_Form/Partner_Form.jsx'
+import Register from './Pages/Register/Register.jsx'
+import InvestorConnect from './Component/Forms/Partner_Investor_Forms/Investor_Form/Investor_Form.jsx'
+import About from './Pages/About/About.jsx' 
+>>>>>>> Stashed changes
 
 function App() {
 
@@ -18,10 +26,23 @@ function App() {
         path='/home'
         element={<Home />}
       />
+      <Route path="/about"
+        element={<About />} 
+      />
       <Route path="/partner-connect"
         element={<PartnerConnect />}
       />
+<<<<<<< Updated upstream
 
+=======
+      <Route path="/investor-connect"
+        element={<InvestorConnect />}
+      />
+      <Route
+        path='/company-register'
+        element={<CompanyDetail />}
+      />
+>>>>>>> Stashed changes
     </Routes>
   )
 }
--- BusinessCommun.com/Client/src/Pages/Home/Home.jsx
+++ BusinessCommun.com/Client/src/Pages/Home/Home.jsx
@@ -2,8 +2,80 @@ import React from 'react'
 
 export default function Home() {
   return (
+<<<<<<< Updated upstream
     <div>
         Home
     </div>
   )
+=======
+    <>
+    <nav className="navbar navbar-expand-lg navbar-style">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          BussinessCommun
+        </a>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarText">
+          {/* LEFT SIDE */}
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a
+                className="nav-link active navText"
+                aria-current="page"
+                href="#"
+              >
+                Companies
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link navText" href="#">
+                Premium Investor
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link navText" href="#">
+                News
+              </a>
+            </li>
+          </ul>
+
+          {/* RIGHT SIDE */}
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link navText" href="#">
+                Contact Us
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link navText" href="/about">
+                About Us
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link navText" href="#">
+                LogOut
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <SlideShow/>
+        </>
+  );
+>>>>>>> Stashed changes
 }
