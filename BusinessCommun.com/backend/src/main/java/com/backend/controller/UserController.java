ebfe22e0a777bac805209ea945b9bdb6da79841e
Amisha Kumari <amishak187@gmail.com>
2026-01-26 18:47:32 +0530

    changes

--- BusinessCommun.com/backend/src/main/java/com/backend/controller/UserController.java
+++ BusinessCommun.com/backend/src/main/java/com/backend/controller/UserController.java
@@ -1,21 +1,18 @@
 package com.backend.controller;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.security.core.Authentication;
 
 import com.backend.dtos.ApiResponseWrapper;
 import com.backend.dtos.AuthRequest;
 import com.backend.dtos.AuthResponse;
 import com.backend.dtos.UserRegistration;
 import com.backend.service.UserService;
-import com.backend.security.JWTUtils;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -26,23 +23,14 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserController {
 
-	private final UserService userService;
-	private final AuthenticationManager authenticationManager;
-	private final JWTUtils jwtUtils;
+	@Autowired
+	private UserService userService;
 
 	@PostMapping("/login")
 	public ResponseEntity<ApiResponseWrapper<AuthResponse>> authenticateUser(@RequestBody @Valid AuthRequest dto) {
 
 		System.out.println("in login " + dto);
-		Authentication fullyAuthenticated = authenticationManager.authenticate(
-				new UsernamePasswordAuthenticationToken(
-						dto.getEmail(),
-						dto.getPassword()
-				)
-			);
-		System.out.println("is user authenticated " + fullyAuthenticated.isAuthenticated());
-		System.out.println("principal " + fullyAuthenticated.getPrincipal());
-		return ResponseEntity.ok(new ApiResponseWrapper<>("success", "Login successful", new AuthResponse("Login successful", jwtUtils.generateToken(fullyAuthenticated))));
+		return ResponseEntity.ok(userService.authenticateUser(dto));
 	}
 
 	@PostMapping("/register")
